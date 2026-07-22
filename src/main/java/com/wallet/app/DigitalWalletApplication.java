d44b07d683ba5aafc5a261006a2f8ca46171de1f
anasniet77 <anaskareem672@gmail.com>
2026-07-22 21:42:29 +0530

    Enable asynchronous processing in DigitalWalletApplication
    Added @EnableAsync annotation to enable asynchronous processing.

--- src/main/java/com/wallet/app/DigitalWalletApplication.java
+++ src/main/java/com/wallet/app/DigitalWalletApplication.java
@@ -2,12 +2,13 @@ package com.wallet.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync; // 👈 1. Import this
 
 @SpringBootApplication
+@EnableAsync // 👈 2. Add this annotation here
 public class DigitalWalletApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DigitalWalletApplication.class, args);
     }
-    
 }
